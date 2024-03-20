d50e11e3f464c4794177e7cb2b6df88cc732558b
Israel <israel_alves77@hotmail.com>
2024-03-20 20:20:56 -0300

    alterações 20/02/2024

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_03_11_170333) do
+ActiveRecord::Schema.define(version: 2024_03_20_225400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 2024_03_11_170333) do
     t.datetime "date_event"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "ticket"
   end
 
   create_table "members", force: :cascade do |t|
@@ -153,6 +154,7 @@ ActiveRecord::Schema.define(version: 2024_03_11_170333) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "tickets", default: []
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,6 @@ models = {
   'Role' => 'Grupo',
   'User' => 'Usuário',
   'Lottery' => 'Loteria',
-  'Quota' => 'Cotas',
   'Member' => 'Clientes'
 }
 
