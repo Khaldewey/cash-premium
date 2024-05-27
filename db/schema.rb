cce3d990d01de59d3b40964472816d966056e01f
israel <Israel@AGENCIAACERTE.LOCAL>
2024-05-27 10:00:26 -0300

    inserção de coluna code_url na tabela members

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_15_195105) do
+ActiveRecord::Schema.define(version: 2024_05_27_125711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 2024_04_15_195105) do
     t.datetime "updated_at", null: false
     t.bigint "lottery_id"
     t.jsonb "tickets", default: {}
+    t.string "qr_code_url"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["lottery_id"], name: "index_members_on_lottery_id"
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
